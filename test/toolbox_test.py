2ce3cef04b6bc4a1033c4977dd547b4bd03bbf24
bmaz <beatrice.mazoyer@gmail.com>
2022-09-07 18:29:42 +0200

    #5 test get_all_images

--- test/toolbox_test.py
+++ test/toolbox_test.py
@@ -1,9 +1,13 @@
+# =============================================================================
+# Pimmi Toolbox Unit Tests
+# =============================================================================
 from os.path import join, dirname
-from pimmi import create_index_mt
+from pimmi.toolbox import get_all_images
 
 SMALL_DATASET_DIR = join(dirname(dirname(__file__)), 'demo_dataset', 'small_dataset')
+NB_IMAGES = 11
 
 
-class TestIndex(object):
-    def test_create_index(self):
-        pass
+class TestToolbox(object):
+    def test_get_all_images(self):
+        assert len(get_all_images([SMALL_DATASET_DIR])) == NB_IMAGES
