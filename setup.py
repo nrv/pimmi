7b8cbbfd4e015519931f0993223a0e55ea9fe75b
bmaz <beatrice.mazoyer@gmail.com>
2022-09-14 18:08:34 +0200

    #6 retry to include config.yml

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='pimmi',
-      version='0.0.6',
+      version='0.0.7',
       description='Python IMage MIning',
       long_description=long_description,
       long_description_content_type='text/markdown',
@@ -15,8 +15,7 @@ setup(name='pimmi',
       keywords='image mining',
       python_requires='>=3.5,<3.8',
       package_data={'pimmi': ['cli/config.yml']},
-      package_dir={'pimmi': 'pimmi'},
-      packages=['pimmi'],
+      packages=find_packages(exclude=["collect*", "dist", "build"]),
       include_package_data=True,
       install_requires=[
         "numpy",
