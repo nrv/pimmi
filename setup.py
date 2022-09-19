07244a04187c0621a90093189fca5d5d91112aa1
bmaz <beatrice.mazoyer@gmail.com>
2022-09-19 17:08:02 +0200

    Bump 0.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='pimmi',
-      version='0.0.7',
+      version='0.0.8',
       description='Python IMage MIning',
       long_description=long_description,
       long_description_content_type='text/markdown',
