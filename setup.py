2d26b504e3f687a14e9bd66f34223931b62f8cfd
bmaz <beatrice.mazoyer@gmail.com>
2023-02-03 18:48:38 +0100

    Bump 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('./README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='pimmi',
-      version='0.1.1',
+      version='0.2.0',
       description='Python IMage MIning',
       long_description=long_description,
       long_description_content_type='text/markdown',
